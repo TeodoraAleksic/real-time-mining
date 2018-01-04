976fc9e0871d7848c7b8525db428aa90ad539f5a
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-01-04 22:40:28 +0100

    Fix sensor init value randomness

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,5 +1,7 @@
 #include "Sensor.h"
 
+#include <stdlib.h>
+
 
 Sensor::Sensor(): Sleepable(50), control(0.0), data(0.0), status(SensorStatus::NONE)
 {
@@ -24,19 +26,19 @@ void Sensor::run()
 void Sensor::readEnv()
 {
 	if (control == 0) {
-		auto now = std::chrono::system_clock::now();
-		auto seed = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+		srand(std::hash<std::thread::id>{}(std::this_thread::get_id()));
+		int number = rand();
 
 		// Generates control between the max and min of the sin function
-		if (seed % 3 == 0)
+		if (number % 3 == 0)
 			// Generates control in range [0, 45]
-			control = (double)(0 + (seed % (45 - 0 + 1)));
-		else if (seed % 3 == 1) 
+			control = (double)(0 + (number % (45 - 0 + 1)));
+		else if (number % 3 == 1)
 			// Generates control in range [135, 225]
-			control = (double)(135 + (seed % (225 - 135 + 1)));
+			control = (double)(135 + (number % (225 - 135 + 1)));
 		else 
 			// Generates control in range [315, 360]
-			control = (double)(315 + (seed % (360 - 315 + 1)));
+			control = (double)(315 + (number % (360 - 315 + 1)));
 	}
 
 	control += 0.5;
