29bc01d891306f4442b641b8d32dedcbcf638a4c
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-01-14 20:17:18 +0100

    Updated Python C functions and GUI

--- python/gui.py
+++ python/gui.py
@@ -2,10 +2,60 @@ import tkinter as tk
 import realtimemining as rtm
 
 
+# Sensor refresh interval
+INTERVAL = 200
+
+
+def water_tank(label: tk.Label):
+    """ Refreshes the water tank level label """
+
+    value = rtm.get_water_level()
+
+    label['text'] = '{}/100'.format(round(value, 2))
+
+    if value <= 20 or value >= 80:
+        label['fg'] = 'red'
+    else:
+        label['fg'] = 'black'
+
+    label.after(INTERVAL, water_tank, label)
+
+
+def water_level(high_level: tk.Label, low_level: tk.Label):
+    """ Refreshes the water level alarms """
+
+    high_level_alarm, low_level_alarm = rtm.get_water_level_alarms()
+
+    if high_level_alarm:
+        high_level['image'] = red
+    else:
+        high_level['image'] = grey
+
+    if low_level_alarm:
+        low_level['image'] = red
+    else:
+        low_level['image'] = grey
+
+    high_level.after(INTERVAL, water_level, high_level, low_level)
+
+
+def pump_control(image: tk.Label):
+    """ Refreshes the pump on/off sign """
+
+    pump_on = rtm.is_pump_on()
+
+    if pump_on:
+        image['image'] = red
+    else:
+        image['image'] = grey
+
+    image.after(INTERVAL, pump_control, image)
+
+
 def sensor(sensor_id: str, label: tk.Label, image: tk.Label):
     """ Refreshes the label and image according to sensor data """
 
-    value, error, alarm = rtm.read_sensor(sensor_id)
+    value, alarm = rtm.get_sensor_data(sensor_id)
 
     label['text'] = '{}/100'.format(round(value, 2))
 
@@ -15,8 +65,13 @@ def sensor(sensor_id: str, label: tk.Label, image: tk.Label):
     else:
         label['fg'] = 'black'
         image['image'] = grey
+
+    if alarm:
+        image['image'] = red
+    else:
+        image['image'] = grey
     
-    label.after(interval, sensor, sensor_id, label, image)
+    label.after(INTERVAL, sensor, sensor_id, label, image)
 
 
 rtm.start_simulation()
@@ -24,9 +79,55 @@ rtm.start_simulation()
 root = tk.Tk()
 root.title('Real-time Mining')
 
+# Alarm images
+grey = tk.PhotoImage(file='grey.png').subsample(4, 4)
+red = tk.PhotoImage(file='red.png').subsample(4, 4)
+
+# Frame for water tank data
+water_frame = tk.Frame(root)
+water_frame.grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+water_frame.columnconfigure(0, weight=1)
+water_frame.rowconfigure(0, weight=1)
+
+# Water tank
+water_tank_label = tk.Label(water_frame, text='Water tank:')
+water_tank_value_label = tk.Label(water_frame, text='0/100')
+
+water_tank_label.grid(row=0, column=0, sticky=(tk.W, tk.E))
+water_tank_value_label.grid(row=0, column=1, sticky=(tk.W, tk.E))
+
+# Water level alarms
+high_level_label = tk.Label(water_frame, text='High level:')
+low_level_label = tk.Label(water_frame, text='Low level:')
+
+high_level_label.grid(row=1, column=0, sticky=(tk.W, tk.E))
+low_level_label.grid(row=1, column=1, sticky=(tk.W, tk.E))
+
+high_level_image = tk.Label(water_frame, image=grey)
+low_level_image = tk.Label(water_frame, image=grey)
+
+high_level_image.grid(row=2, column=0, sticky=(tk.W, tk.E))
+low_level_image.grid(row=2, column=1, sticky=(tk.W, tk.E))
+
+# Pump controls
+pump_label = tk.Label(water_frame, text='Pump on/off:')
+pump_label.grid(row=3, column=0, sticky=(tk.W, tk.E))
+
+pump_image = tk.Label(water_frame, image=grey)
+pump_image.grid(row=3, column=1, sticky=(tk.W, tk.E))
+
+# Water tank frame padding
+for child in water_frame.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+# Reads water tank and pump control data
+water_tank_value_label.after(INTERVAL, water_tank, water_tank_value_label)
+high_level_image.after(INTERVAL, water_level, high_level_image, low_level_image)
+pump_image.after(INTERVAL, pump_control, pump_image)
+
 # Frame for the sensor data
 sensor_frame = tk.Frame(root)
-sensor_frame.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+sensor_frame.grid(row=0, column=1, sticky=(tk.N, tk.W, tk.E, tk.S))
 sensor_frame.columnconfigure(0, weight=1)
 sensor_frame.rowconfigure(0, weight=1)
 
@@ -46,10 +147,6 @@ ch4_label.grid(row=2, column=1, sticky=tk.E)
 co_label.grid(row=3, column=1, sticky=tk.E)
 air_label.grid(row=4, column=1, sticky=tk.E)
 
-# Sensor alarm images
-grey = tk.PhotoImage(file='grey.png').subsample(4, 4)
-red = tk.PhotoImage(file='red.png').subsample(4, 4)
-
 # Sensor alarms
 ch4_image = tk.Label(sensor_frame, image=grey)
 co_image = tk.Label(sensor_frame, image=grey)
@@ -68,16 +165,14 @@ ch4_value.grid(row=2, column=3, sticky=(tk.W, tk.E))
 co_value.grid(row=3, column=3, sticky=(tk.W, tk.E))
 air_value.grid(row=4, column=3, sticky=(tk.W, tk.E))
 
+# Sensor frame padding
 for child in sensor_frame.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
-# Sensor refresh interval
-interval = 200
-
 # Reads sensor data and refreshes labels and images
-ch4_value.after(interval, sensor, 'CH4', ch4_value, ch4_image)
-co_value.after(interval, sensor, 'CO', co_value, co_image)
-air_value.after(interval, sensor, 'AIR_FLOW', air_value, air_image)
+ch4_value.after(INTERVAL, sensor, 'CH4', ch4_value, ch4_image)
+co_value.after(INTERVAL, sensor, 'CO', co_value, co_image)
+air_value.after(INTERVAL, sensor, 'AIR_FLOW', air_value, air_image)
 
 root.mainloop()
 
--- src/realtimemining.cpp
+++ src/realtimemining.cpp
@@ -1,12 +1,12 @@
 #include "Python.h"
-#include "EnvironmentMonitor.h"
+#include "../real-time-mining/Simulation.h"
 
 #include <string>
 
 using namespace std;
 
 
-EnvironmentMonitor em;
+Simulation sm;
 
 
 static PyObject *SensorIDError;
@@ -14,22 +14,56 @@ static PyObject *SensorIDError;
 
 PyObject* startSimulation(PyObject *self, PyObject *args)
 {
-	em.start();
+	sm.start();
 
 	Py_INCREF(Py_None);
 	return Py_None;
 }
 
 
-PyObject* readSensor(PyObject *self, PyObject *args)
+PyObject* setSensorThreshold(PyObject *self, PyObject *args)
+{
+	const char *argSensorID;
+	double threshold;
+
+	// Parses arguments
+	if (!PyArg_ParseTuple(args, "sf", &argSensorID, &threshold))
+		return NULL;
+
+	EnvironmentMonitor::SensorID sensorID;
+
+	// Converts string to sensor ID
+	if (std::string(argSensorID).compare("CH4") == 0)
+		sensorID = EnvironmentMonitor::SensorID::CH4;
+	else if (std::string(argSensorID).compare("CO") == 0)
+		sensorID = EnvironmentMonitor::SensorID::CO;
+	else if (std::string(argSensorID).compare("AIR_FLOW") == 0)
+		sensorID = EnvironmentMonitor::SensorID::AIR_FLOW;
+	else
+	{
+		PyErr_SetString(SensorIDError, ("Unrecognized sensor ID " + std::string(argSensorID)).c_str());
+		return NULL;
+	}
+
+	// C++ function call
+	sm.setSensorThreshold(sensorID, threshold);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+PyObject* getSensorData(PyObject *self, PyObject *args)
 {
 	const char *arg;
 
+	// Parses arguments
 	if (!PyArg_ParseTuple(args, "s", &arg))
 		return NULL;
 
 	EnvironmentMonitor::SensorID sensorID;
 
+	// Converts string to sensor ID
 	if (std::string(arg).compare("CH4") == 0)
 		sensorID = EnvironmentMonitor::SensorID::CH4;
 	else if (std::string(arg).compare("CO") == 0)
@@ -43,25 +77,120 @@ PyObject* readSensor(PyObject *self, PyObject *args)
 	}
 
 	double value;
-	bool error;
 	bool alarm;
 
-	em.getSensorData(sensorID, value, error, alarm);
+	// C++ function call
+	sm.getSensorData(sensorID, value, alarm);
 
-	PyObject* tuple = PyTuple_New(3);
+	// Returns tuple
+	PyObject* tuple = PyTuple_New(2);
 
 	PyTuple_SetItem(tuple, 0, PyFloat_FromDouble(value));
-	PyTuple_SetItem(tuple, 1, PyBool_FromLong(error ? 1 : 0));
-	PyTuple_SetItem(tuple, 2, PyBool_FromLong(alarm ? 1 : 0));
+	PyTuple_SetItem(tuple, 1, PyBool_FromLong(alarm ? 1 : 0));
 
 	return tuple;
 }
 
 
-// Defines the method of the module
+PyObject* getPumpAlarm(PyObject *self, PyObject *args)
+{
+	return PyBool_FromLong(sm.getPumpAlarm() ? 1 : 0);
+}
+
+
+PyObject* isPumpOn(PyObject *self, PyObject *args)
+{
+	return PyBool_FromLong(sm.isPumpOn() ? 1 : 0);
+}
+
+
+PyObject* turnPumpOn(PyObject *self, PyObject *args)
+{
+	sm.turnPumpOn();
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+PyObject* turnPumpOff(PyObject *self, PyObject *args)
+{
+	sm.turnPumpOff();
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+PyObject* getWaterLevelAlarms(PyObject *self, PyObject *args)
+{
+	bool highLevelAlarm;
+	bool lowLevelAlarm;
+
+	// C++ function call
+	sm.getWaterLevelAlarms(highLevelAlarm, lowLevelAlarm);
+
+	// Returns tuple
+	PyObject* tuple = PyTuple_New(2);
+
+	PyTuple_SetItem(tuple, 0, PyBool_FromLong(highLevelAlarm ? 1 : 0));
+	PyTuple_SetItem(tuple, 1, PyBool_FromLong(lowLevelAlarm ? 1 : 0));
+
+	return tuple;
+}
+
+
+PyObject* setHighLevelThreshold(PyObject *self, PyObject *args)
+{
+	double threshold;
+
+	// Parses arguments
+	if (!PyArg_ParseTuple(args, "f", &threshold))
+		return NULL;
+
+	// C++ function call
+	sm.setHighLevelThreshold(threshold);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+PyObject* setLowLevelThreshold(PyObject *self, PyObject *args)
+{
+	double threshold;
+
+	// Parses arguments
+	if (!PyArg_ParseTuple(args, "f", &threshold))
+		return NULL;
+
+	// C++ function call
+	sm.setLowLevelThreshold(threshold);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+PyObject* getWaterLevel(PyObject *self, PyObject *args)
+{
+	return PyFloat_FromDouble(sm.getWaterLevel());
+}
+
+
+// Defines the methods of the module
 static PyMethodDef realTimeMining[] = {      
 	{ "start_simulation", (PyCFunction)startSimulation, METH_NOARGS, "Starts the real-time mining simulation" },
-	{ "read_sensor", (PyCFunction)readSensor, METH_VARARGS, "Reads current value of specified sensor" },
+	{ "set_sensor_threshold", (PyCFunction)setSensorThreshold, METH_VARARGS, "Sets the threshold of the specified sensor" },
+	{ "get_sensor_data", (PyCFunction)getSensorData, METH_VARARGS, "Gets data of the specified sensor" },
+	{ "get_pump_alarm", (PyCFunction)getPumpAlarm, METH_NOARGS, "Returns if the pump alarm is on" },
+	{ "is_pump_on", (PyCFunction)isPumpOn, METH_NOARGS, "Returns if the pump is on" },
+	{ "turn_pump_on", (PyCFunction)turnPumpOn, METH_NOARGS, "Turns the pump on" },
+	{ "turn_pump_off", (PyCFunction)turnPumpOff, METH_NOARGS, "Turns the pump off" },
+	{ "get_water_level_alarms", (PyCFunction)getWaterLevelAlarms, METH_NOARGS, "Returns the water level alarms" },
+	{ "set_high_level_threshold", (PyCFunction)setHighLevelThreshold, METH_VARARGS, "Sets the high water level threshold" },
+	{ "set_low_level_threshold", (PyCFunction)setLowLevelThreshold, METH_VARARGS, "Sets the low water level threshold" },
+	{ "get_water_level", (PyCFunction)getWaterLevel, METH_NOARGS, "Returns the water level" },
 	{ nullptr, nullptr, 0, nullptr }
 };
 
